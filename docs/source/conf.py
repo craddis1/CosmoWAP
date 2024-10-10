759c34b02bc4215314db501639585b35ed2f07f8
Chris <chrisaddis@live.com>
2024-10-10 22:41:51 +0100

    readthedocsagain

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,6 @@ pygments_style = None
 # a list of builtin themes.
 # CA: changing theme here....
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
