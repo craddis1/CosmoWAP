729e0bfa6f79d81575812734b720e727ffe54546
craddis1 <chrisaddis@live.com>
2025-05-29 17:21:16 +0100

    minor changes

--- src/cosmo_wap/lib/utils.py
+++ src/cosmo_wap/lib/utils.py
@@ -11,7 +11,7 @@ sigma8 = 0.828
 n_s = 0.968
 """
 
-def get_cosmology(h = 0.6766,Omega_m = 0.30964144,Omega_cdm = 0.11933,A_s = 2.105e-9,n_s = 0.9665,k_max=10,sigma8=None):
+def get_cosmology(h = 0.6766,Omega_m = 0.30964144,Omega_b = 0.02242,A_s = 2.105e-9,n_s = 0.9665,k_max=10,sigma8=None):
     #h = 0.6766,Omega_b = 0.02242,Omega_cdm = 0.11933,A_s = 2.105e-9,n_s = 0.9665,k_max=10,sigma8=None
     """ calls class for some set of parameters and returns the cosmology - base cosmology is planck 2018"""
     Omega_b *= 1/h**2
@@ -25,7 +25,7 @@ def get_cosmology(h = 0.6766,Omega_m = 0.30964144,Omega_cdm = 0.11933,A_s = 2.10
                  'Omega_cdm': Omega_m-Omega_b,#Omega_cdm
                  'h':h,
                  'n_s':n_s,
-                 'A_s':A_s,#'n_s':n_s,'sigma8':0.828,#
+                 'A_s':A_s,#'n_s':n_s,#
                  'P_k_max_1/Mpc':k_max,
                  'z_max_pk':10. #Default value is 10
     }
@@ -36,7 +36,7 @@ def get_cosmology(h = 0.6766,Omega_m = 0.30964144,Omega_cdm = 0.11933,A_s = 2.10
                  'Omega_cdm':Omega_m-Omega_b,#Omega_cdm,#
                  'h':h,
                  'n_s':n_s,
-                 'sigma8':sigma8,#'A_s':A_s,#'n_s':n_s,
+                 'sigma8':sigma8,#'A_s':A_s,'sigma8':0.828,
                  'P_k_max_1/Mpc':k_max,
                  'z_max_pk':10. #Default value is 10
                  }
