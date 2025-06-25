2eedfe3980fcc9a14d0811c5cf8997a87a8a740d
craddis1 <chrisaddis@live.com>
2025-06-25 09:48:46 +0100

    update gitignore

--- src/cosmo_wap/pk/PNG.py
+++ src/cosmo_wap/pk/PNG.py
@@ -7,16 +7,16 @@ class BaseFNL:
     def _l(mu,cosmo_funcs,k1,zz=0,t=0,fNL=1, fNL_type='Loc'):
         Pk,f,D1,b1,xb1,b01,Mk1,xb01 = cosmo_funcs.unpack_pk(k1,zz,fNL_type=fNL_type)
         
-        return D1**2*Pk*fNL*(b01*(f*mu**2 + xb1) + xb01*(b1 + f*mu**2))/Mk1
+        return D1**2*Pk*(5*Mk1*b1*(Mk1*f + 3*Mk1*xb1 + 3*fNL*xb01) + Mk1*f*(3*Mk1*f + 5*Mk1*xb1 + 5*fNL*xb01) + 5*b01*fNL*(Mk1*f + 3*Mk1*xb1 + 3*fNL*xb01))/(15*Mk1**2)
     
     @staticmethod
     def _l0(cosmo_funcs,k1,zz=0,t=0,sigma=None,fNL=1, fNL_type='Loc'):
         Pk,f,D1,b1,xb1,b01,Mk1,xb01 = cosmo_funcs.unpack_pk(k1,zz,fNL_type=fNL_type)
         
-        expr = D1**2*Pk*fNL*(b01*(f + 3*xb1) + xb01*(3*b1 + f))/(3*Mk1)
+        expr =D1**2*Pk*fNL*(Mk1*xb01*(3*b1 + f) + b01*(Mk1*f + 3*Mk1*xb1 + 3*fNL*xb01))/(3*Mk1**2)
         
         if sigma != None:
-            expr = D1**2*Pk*fNL*(-2*f*k1*sigma*(b01 + xb01) + np.sqrt(2)*np.sqrt(np.pi)*(b01*(f + k1**2*sigma**2*xb1) + xb01*(b1*k1**2*sigma**2 + f))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(2*Mk1*k1**3*sigma**3)
+            expr = D1**2*Pk*fNL*(-2*Mk1*f*k1*sigma*(b01 + xb01) + np.sqrt(2)*np.sqrt(np.pi)*(Mk1*b01*f + Mk1*xb01*(b1*k1**2*sigma**2 + f) + b01*k1**2*sigma**2*(Mk1*xb1 + fNL*xb01))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(2*Mk1**2*k1**3*sigma**3)
         return expr
     
     @staticmethod
@@ -26,7 +26,7 @@ class BaseFNL:
         expr = 2*D1**2*Pk*f*fNL*(b01 + xb01)/(3*Mk1)
         
         if sigma != None:
-            expr = -5*D1**2*Pk*fNL*(2*k1*sigma*(b01*f*(2*k1**2*sigma**2 + 9) + 3*b01*k1**2*sigma**2*xb1 + 3*b1*k1**2*sigma**2*xb01 + f*xb01*(2*k1**2*sigma**2 + 9)) + np.sqrt(2)*np.sqrt(np.pi)*(b01*f*(k1**2*sigma**2 - 9) + b01*k1**2*sigma**2*xb1*(k1**2*sigma**2 - 3) + b1*k1**2*sigma**2*xb01*(k1**2*sigma**2 - 3) + f*xb01*(k1**2*sigma**2 - 9))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(4*Mk1*k1**5*sigma**5)
+            expr = -5*D1**2*Pk*fNL*(2*k1*sigma*(Mk1*b01*f*(2*k1**2*sigma**2 + 9) + 3*Mk1*b1*k1**2*sigma**2*xb01 + Mk1*f*xb01*(2*k1**2*sigma**2 + 9) + 3*b01*k1**2*sigma**2*(Mk1*xb1 + fNL*xb01)) + np.sqrt(2)*np.sqrt(np.pi)*(Mk1*b01*f*(k1**2*sigma**2 - 9) + Mk1*b1*k1**2*sigma**2*xb01*(k1**2*sigma**2 - 3) + Mk1*f*xb01*(k1**2*sigma**2 - 9) + b01*k1**2*sigma**2*(Mk1*xb1 + fNL*xb01)*(k1**2*sigma**2 - 3))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(4*Mk1**2*k1**5*sigma**5)
         
         return expr
 
@@ -46,25 +46,25 @@ class Loc:
 class Eq:
     @staticmethod
     def l(mu,cosmo_funcs,k1,zz=0,t=0,fNL=1):
-        return k1**2 * BaseFNL._l(mu,cosmo_funcs,k1,zz=zz,t=t,fNL=fNL,fNL_type='Eq')
+        return BaseFNL._l(mu,cosmo_funcs,k1,zz=zz,t=t,fNL= k1**2 *fNL,fNL_type='Eq')
         
     @staticmethod
     def l0(cosmo_funcs,k1,zz=0,t=0,sigma=None,fNL=1):
-        return k1**2 * BaseFNL._l0(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=fNL,fNL_type='Eq')
+        return BaseFNL._l0(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=k1**2 *fNL,fNL_type='Eq')
     
     @staticmethod
     def l2(cosmo_funcs,k1,zz=0,t=0,sigma=None,fNL=1):
-        return k1**2 * BaseFNL._l2(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=fNL,fNL_type='Eq')
+        return BaseFNL._l2(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=k1**2 *fNL,fNL_type='Eq')
     
 class Orth:
     @staticmethod
     def l(mu,cosmo_funcs,k1,zz=0,t=0,fNL=1):
-        return k1 * BaseFNL._l(mu,cosmo_funcs,k1,zz=zz,t=t,fNL=fNL,fNL_type='Orth')
+        return BaseFNL._l(mu,cosmo_funcs,k1,zz=zz,t=t,fNL=k1 *fNL,fNL_type='Orth')
         
     @staticmethod
     def l0(cosmo_funcs,k1,zz=0,t=0,sigma=None,fNL=1):
-        return k1 * BaseFNL._l0(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=fNL,fNL_type='Orth')
+        return BaseFNL._l0(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=k1 *fNL,fNL_type='Orth')
     
     @staticmethod
     def l2(cosmo_funcs,k1,zz=0,t=0,sigma=None,fNL=1):
-        return k1 * BaseFNL._l2(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=fNL,fNL_type='Orth')
+        return BaseFNL._l2(cosmo_funcs,k1,zz=zz,t=t,sigma=sigma,fNL=k1 *fNL,fNL_type='Orth')
--- src/cosmo_wap/pk/Pk_0.py
+++ src/cosmo_wap/pk/Pk_0.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.special import erf  # Error function needed from integral over FoG
 
 #1st order terms
-class NPP:    
+class NPP:
+    @staticmethod
     def l0(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         #so we could always change to work with a namespace object or locals
         #unpack all necessary terms
@@ -15,6 +16,7 @@ class NPP:
 
         return expr
     
+    @staticmethod
     def l2(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         Pk,f,D1,b1,xb1 = cosmo_funcs.unpack_pk(k1,zz)
         
@@ -24,9 +26,20 @@ class NPP:
             expr = -5*D1**2*Pk*(2*k1*sigma*(3*b1*k1**4*sigma**4*xb1 + f**2*(2*k1**4*sigma**4 + 12*k1**2*sigma**2 + 45) + f*k1**2*sigma**2*(b1 + xb1)*(2*k1**2*sigma**2 + 9)) + np.sqrt(2)*np.sqrt(np.pi)*(b1*k1**4*sigma**4*xb1*(k1**2*sigma**2 - 3) + 3*f**2*(k1**2*sigma**2 - 15) + f*k1**2*sigma**2*(b1 + xb1)*(k1**2*sigma**2 - 9))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(4*k1**7*sigma**7)
         return expr
     
+    @staticmethod
+    def l4(cosmo_funcs,k1,zz=0,t=0,sigma=None):
+        Pk,f,D1,b1,xb1 = cosmo_funcs.unpack_pk(k1,zz)
+        
+        expr = 8*D1**2*Pk*f**2/35
+        
+        if sigma != None:
+            expr = -9*D1**2*Pk*(10*b1**2*k1**5*sigma**5*(k1**2*sigma**2 + 21) + 4*b1*f*k1**3*sigma**3*(8*k1**4*sigma**4 + 85*k1**2*sigma**2 + 525) + 2*f**2*k1*sigma*(8*k1**6*sigma**6 + 104*k1**4*sigma**4 + 775*k1**2*sigma**2 + 3675) - 3*np.sqrt(2)*np.sqrt(np.pi)*(b1**2*k1**4*sigma**4*(k1**4*sigma**4 - 10*k1**2*sigma**2 + 35) + 2*b1*f*k1**2*sigma**2*(k1**4*sigma**4 - 30*k1**2*sigma**2 + 175) + f**2*(3*k1**4*sigma**4 - 150*k1**2*sigma**2 + 1225))*erf(np.sqrt(2)*k1*sigma/2)*np.exp(k1**2*sigma**2/2))*np.exp(-k1**2*sigma**2/2)/(16*k1**9*sigma**9)
+        return expr
+    
     
 #1st order terms
-class GR1:        
+class GR1:
+    @staticmethod
     def l1(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         Pk,f,D1,b1,xb1,gr1,_,xgr1,_ = cosmo_funcs.unpack_pk(k1,zz,GR=True)
         
@@ -37,6 +50,7 @@ class GR1:
             
         return expr
     
+    @staticmethod
     def l3(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         Pk,f,D1,b1,xb1,gr1,_,xgr1,_ = cosmo_funcs.unpack_pk(k1,zz,GR=True)
         
@@ -49,7 +63,8 @@ class GR1:
     
     
 #2nd order terms
-class GR2:        
+class GR2:
+    @staticmethod
     def l0(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         Pk,f,D1,b1,xb1,gr1,gr2,xgr1,xgr2 = cosmo_funcs.unpack_pk(k1,zz,GR=True)
         
@@ -60,6 +75,7 @@ class GR2:
         
         return expr
     
+    @staticmethod
     def l2(cosmo_funcs,k1,zz=0,t=0,sigma=None):
         Pk,f,D1,b1,xb1,gr1,gr2,xgr1,xgr2 = cosmo_funcs.unpack_pk(k1,zz,GR=True)
         
