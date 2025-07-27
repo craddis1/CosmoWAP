e0f3e3c65c4c4b123338b6816f04bfb68d2cbc17
craddis1 <chrisaddis@live.com>
2025-07-27 22:01:16 +0100

    add mpirun file

---
+++ src/cosmo_wap/forecast/mpi_mcmc.py
@@ -0,0 +1,38 @@
+"""
+Example run:
+mpirun -n 20 python3 mpi_mcmc.py
+"""
+
+from mpi4py import MPI
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+import cosmo_wap as cw
+from cosmo_wap.lib import utils
+
+# --- MPI setup ---
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank() # The process ID (e.g., 0, 1, 2, ...)
+size = comm.Get_size() # The total number of processes
+
+# --- Each process does some work ---
+
+#get planck comsology
+cosmo = utils.get_cosmo(k_max=10)
+#load preset surveys
+survey_params = cw.SurveyParams(cosmo)
+
+# get main class object - this is dependent on cosmology and survey so we will gather for different surveys
+cosmo_funcs = cw.ClassWAP(cosmo,survey_params.Euclid,compute_bias=False,emulator=True)
+# define k_max_func for forecasts
+kmax_func = lambda zz: 0.1 *cosmo_funcs.h*(1+zz)**(2/(2+cosmo_funcs.n_s))
+pkln=[0,2]
+
+forecast = cw.forecast.FullForecast(cosmo_funcs,kmax_func=kmax_func,N_bins=10)
+sampler = cw.forecast.Sampler(forecast,['fNL','n_s','GR2','A_s'],terms=['Loc','NPP'],bias_list=['IntInt'],pkln=[0],R_stop=0.005)
+sampler.run()
+
+# ONLY rank 0 has the full results and is allowed to write to the disk
+if rank == 0:
+    sampler.save("sampler.pkl")
